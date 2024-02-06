7dc145a50309f5fc0047aba4f393004b785e4a30
Danil <70006380+TheNorth322@users.noreply.github.com>
2024-02-06 15:23:04 +0700

    init project, create topbar menu, add file structure

---
+++ Compiler/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Compiler
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            this.MainWindow = new CompilerWindow();
+            MainWindow.Show();
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ Compiler/utils/ViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Compiler.utils;
+
+public class ViewModelBase : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public ViewModelBase()
+    {
+    }
+    
+       
+    protected virtual void OnPropertyChanged(string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected virtual void Dispose()
+    {
+    }
+}
